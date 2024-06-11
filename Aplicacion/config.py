6b52fbed016458f1f854f24a4f7a05d8a16925b9
Felix Cruz Tapia <felixcruztapia@gmail.com>
2024-06-11 16:58:25 -0300

    Correcion de archivo SQL y Cambio de nombre

--- Aplicacion/config.py
+++ Aplicacion/config.py
@@ -1,5 +1,9 @@
+"""
+Este es un archivo de configuracion. Aqui debe cargar las credenciales de la base de datos.
+"""
+
 
 config = {'host': 'localhost',
           'user': 'root',
-          'password': 'MYSQL20242310tecdc',
-          'database': 'empleadoss_departamentoss'}
+          'password': '',
+          'database': 'proyecto_aulas'}
